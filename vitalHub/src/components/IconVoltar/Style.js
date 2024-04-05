03f6abf0d8c13f7b571419720706a1aa976f02b2
Gustavo Enzo Santos Aguilar <49816214857@info.local>
2024-04-05 16:24:08 -0300

    .

--- vitalHub/src/components/IconVoltar/Style.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const IconVoltar = styled.View`
-width: 90%;
-margin-top: 23px;
-`
--- vitalHub/src/components/Logo/Style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-
-export const Logo = styled.Image`
-margin: 20px 0px;
-width: 214px;
-height: 120px;
-
-`
